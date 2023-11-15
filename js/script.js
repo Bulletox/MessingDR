b09899f225d5a772fc149c7314fee22eb93ed1ff
nandyys <nandyyandrade@gmail.com>
2023-11-15 09:56:26 +0100

    Add second album

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
+
+  document.addEventListener("DOMContentLoaded", function () {
     // Oculta el segundo álbum al cargar la página
     document.getElementById("secondAlbum").style.display = "none";
 
     // Agrega un evento de clic al botón "Ver más"
     document.getElementById("verMasBtn").addEventListener("click", function () {
-      // Muestra el segundo álbum cuando se hace clic
-      document.getElementById("secondAlbum").style.display = "block";
+      // Muestra u oculta el segundo álbum cuando se hace clic
+      var secondAlbum = document.getElementById("secondAlbum");
+      secondAlbum.style.display = (secondAlbum.style.display === "none") ? "block" : "none";
     });
   });
+
