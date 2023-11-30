828c2193ffac50a3da56c50d611dac322a6fa203
Marc <marc.tamarit.02@gmail.com>
2023-11-30 13:05:57 +0100

    add cambio en conexion

--- phps/Conexion_MySQLi.php
+++ phps/Conexion_MySQLi.php
@@ -15,72 +15,74 @@ $user = "mymessing97";
 $password = "VNfHYGt3";
 $dbname = "messingsql";
 
+// Establecer conexión
+$conn = new mysqli($servername, $user, $password, $dbname);
 
-    // Establecer conexión
-    $conn = new mysqli($servername, $user, $password, $dbname);
+// Verificación de la conexión
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
 
-    // Verificación de la conexión
-    if ($conn->connect_error) {
-        die("Conexión fallida: " . $conn->connect_error);
-    }
-    try {
-        // Procesamiento del formulario que se ha enviado
-        if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            // Validación y limpieza de los datos del formulario
-            $nombre = limpiar_datos($_POST["nombre"]);
-            $correo = limpiar_datos($_POST["email"]);
-            $fecha = limpiar_datos($_POST["fecha"]);
-            $hora = limpiar_datos($_POST["hora"]);
-            $num_personas = limpiar_datos($_POST["cantidad"]);
-    
-            // Verificar si el correo ya está registrado
-            $sqlVerificarCorreo = "SELECT id_usuario FROM usuario WHERE correo = ?";
-            $stmtVerificarCorreo = $conn->prepare($sqlVerificarCorreo);
-            $stmtVerificarCorreo->bind_param("s", $correo);
-            $stmtVerificarCorreo->execute();
-            $stmtVerificarCorreo->store_result();
-    
-            // Obtener el ID del usuario existente o insertar uno nuevo
-            if ($stmtVerificarCorreo->num_rows > 0) {
-                $stmtVerificarCorreo->bind_result($id_usuario);
-                $stmtVerificarCorreo->fetch();
-            } else {
-                // El correo no existe, insertar en la tabla "Usuario"
-                $sqlUsuario = "INSERT INTO usuario (nombre, correo) VALUES (?, ?)";
-                $stmtUsuario = $conn->prepare($sqlUsuario);
-                $stmtUsuario->bind_param("ss", $nombre, $correo);
-    
-                if (!$stmtUsuario->execute()) {
-                    echo "Error al insertar en la tabla Usuario: " . $stmtUsuario->error;
-                    $conn->close();
-                    exit;
-                }
-    
-                // Obtener el ID del usuario recién insertado
-                $id_usuario = $stmtUsuario->insert_id;
+try {
+    // Procesamiento del formulario que se ha enviado
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Validación y limpieza de los datos del formulario
+        $nombre = limpiar_datos($_POST["nombre"]);
+        $correo = limpiar_datos($_POST["email"]);
+        $fecha = limpiar_datos($_POST["fecha"]);
+        $hora = limpiar_datos($_POST["hora"]);
+        $num_personas = limpiar_datos($_POST["cantidad"]);
+
+        // Verificar si el correo ya está registrado
+        $sqlVerificarCorreo = "SELECT id_usuario FROM usuario WHERE correo = ?";
+        $stmtVerificarCorreo = $conn->prepare($sqlVerificarCorreo);
+        $stmtVerificarCorreo->bind_param("s", $correo);
+        $stmtVerificarCorreo->execute();
+        $stmtVerificarCorreo->store_result();
+
+        // Obtener el ID del usuario existente o insertar uno nuevo
+        if ($stmtVerificarCorreo->num_rows > 0) {
+            $stmtVerificarCorreo->bind_result($id_usuario);
+            $stmtVerificarCorreo->fetch();
+        } else {
+            // El correo no existe, insertar en la tabla "Usuario"
+            $sqlUsuario = "INSERT INTO usuario (nombre, correo) VALUES (?, ?)";
+            $stmtUsuario = $conn->prepare($sqlUsuario);
+            $stmtUsuario->bind_param("ss", $nombre, $correo);
+
+            if (!$stmtUsuario->execute()) {
+                echo "Error al insertar en la tabla Usuario: " . $stmtUsuario->error;
+                $conn->close();
+                exit;
             }
-    
-            // ...
-    
-            // Creado y ejecutado la query para la inserción de los datos en la tabla "Reservas"
-            $sqlReservas = "INSERT INTO reservas (fecha, num_personas, estado, id_usuario, hora) VALUES (?, ?, ?, ?, ?)";
-            $stmtReservas = $conn->prepare($sqlReservas);
-    
-            // Vinculación de los datos para la tabla "Reservas"
-            $stmtReservas->bind_param("siiis", $fecha, $num_personas, $estado, $id_usuario, $hora);
-    
-            // Ejecutar la consulta para la tabla "Reservas"
-            if ($stmtReservas->execute()) {
-                echo "Reserva exitosa";
-            } else {
-                echo "Error al insertar en la tabla Reservas: " . $stmtReservas->error;
-            }
+
+            // Obtener el ID del usuario recién insertado
+            $id_usuario = $stmtUsuario->insert_id;
         }
-    
-        // ...
-    } catch (Exception $e) {
-        echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+
+        // Definir un estado predeterminado (ajústalo según tus necesidades)
+        $estado = ($num_personas > 10) ? 2 : 1;
+
+        // Creado y ejecutado la query para la inserción de los datos en la tabla "Reservas"
+        $sqlReservas = "INSERT INTO reservas (fecha, num_personas, estado, id_usuario, hora) VALUES (?, ?, ?, ?, ?)";
+        $stmtReservas = $conn->prepare($sqlReservas);
+
+        // Vinculación de los datos para la tabla "Reservas"
+        $stmtReservas->bind_param("siiis", $fecha, $num_personas, $estado, $id_usuario, $hora);
+
+        // Ejecutar la consulta para la tabla "Reservas"
+        if ($stmtReservas->execute()) {
+            echo "Reserva exitosa";
+        } else {
+            echo "Error al insertar en la tabla Reservas: " . $stmtReservas->error;
+        }
     }
+
+    // Cerrar conexión
+    $conn->close();
+} catch (Exception $e) {
+    echo 'Excepción capturada: ',  $e->getMessage(), "\n";
+}
 
 header("Location: ../index.html");
 ?>
