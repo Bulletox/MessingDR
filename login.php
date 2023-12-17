9b546b155fa6b78313c055095596d21ca932a32e
nandyys <nandyyandrade@gmail.com>
2023-12-17 21:29:41 +0100

    add boton hacia atrás

--- login.php
+++ login.php
@@ -11,13 +11,22 @@
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
+    
     <link rel="stylesheet" href="styleslogin.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
 </head>
 
 <body class="bg-gradient-primary no-scroll">
-
+<a href="index.html" class="btn btn-primary " style="position: absolute; top: 50px; left: 50px; background-color:#198754; border-color:black; ">
+        <i class="fas fa-arrow-left"></i>
+    </a>
     <section class="user">
+ 
+        
         <div class="user_options-container">
+
             <div class="user_options-text">
                 <div class="user_options-unregistered">
                     <h2 class="user_unregistered-title">¿Aún no formas parte de Messing?</h2>
@@ -32,6 +41,7 @@
                     <button class="user_registered-login" id="login-button">Login</button>
                 </div>
             </div>
+ 
 
             <div class="user_options-forms" id="user_options-forms">
                 <div class="user_forms-login">
@@ -59,6 +69,8 @@
                     </form>
 
                 </div>
+
+
                 <div class="user_forms-signup">
                     <h2 class="forms_title">Sign Up</h2>
                     <form action="phps/singin.php" method="post" class="forms_form">
@@ -81,6 +93,8 @@
             </div>
         </div>
     </section>
+ 
+
 
     <script>
         const signupButton = document.getElementById('signup-button'),
@@ -102,6 +116,7 @@
         signupSubmitButton.addEventListener('click', () => {
             window.location.href = 'indexPC.html';
         }, false);
+        
     </script>
 
     <script src="vendor/jquery/jquery.min.js"></script>
@@ -110,6 +125,8 @@
     <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
     <script src="js/sb-admin-2.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 
